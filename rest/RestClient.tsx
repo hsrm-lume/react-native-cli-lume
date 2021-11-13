ce84e50f4edf3bcedf90d5a317ca6a75db41a673
lmaos-txt <code@lmaos.de>
2021-11-13 13:57:43 +0100

    removed restClient constructor and made methods static for now

--- rest/RestClient.tsx
+++ rest/RestClient.tsx
@@ -1,26 +1,24 @@
 import axios from 'axios';
 class restClient {
-    constructor(baseUrl: string) {}
-
-    get(route: string, query?: object) {
+    static get(route: string, query?: object) {
         return axios.get(route, query).catch(function (e) {
             console.warn(e);
         });
     }
 
-    post(route: string, body?: object) {
+    static post(route: string, body?: object) {
         return axios.post(route, body).catch(function (e) {
             console.warn(e);
         });
     }
 
-    put(route: string, body?: object) {
+    static put(route: string, body?: object) {
         return axios.put(route, body).catch(function (e) {
             console.warn(e);
         });
     }
 
-    delete(route: string, query?: object) {
+    static delete(route: string, query?: object) {
         return axios.delete(route, query).catch(function (e) {
             console.warn(e);
         });
