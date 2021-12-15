da8986b510ff7f8ea36b9e01d67ee404d13f122b
mptr <54647612+mptr@users.noreply.github.com>
2021-12-15 13:06:11 +0100

    fix remove file from barrel

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,5 +3,4 @@ export * from './GeoLocation';
 export * from './HandledPromise';
 export * from './TranmissionData';
 export * from './UserData';
-export * from './useOnDepUpdate';
 export * from './useOnInit';
