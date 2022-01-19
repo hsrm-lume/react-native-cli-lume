88ff1a9b8b7cef9bbefd637f8c597fa0e77c236d
roschnie <robert.rschnieders@gmail.com>
2022-01-19 01:22:30 +0100

    qrScanner works

--- src/components/qr/qrScanner.tsx
+++ src/components/qr/qrScanner.tsx
@@ -1,22 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 import {
 	Camera,
 	CameraPermissionStatus,
 	useCameraDevices,
-	useFrameProcessor,
 } from 'react-native-vision-camera';
 import {
 	useScanBarcodes,
-	scanBarcodes,
 	BarcodeFormat,
+	Barcode,
 } from 'vision-camera-code-scanner';
 import {RestClient, writeUserData} from '../../services';
 import {GeoLocation, HandledPromise} from '../../types';
 import {QrCodeData} from '../../types/TranmissionData';
 import ThinCross from '../../assets/thinCross.svg';
 import {Icon} from '../error/icon';
-import {runOnJS} from 'react-native-reanimated';
 
 const QRScanner = (props: {
 	uid: string;
@@ -26,10 +24,7 @@ const QRScanner = (props: {
 	const [cameraPermissionStatus, setCameraPermissionStatus] =
 		useState<CameraPermissionStatus>('not-determined');
 
-	Camera.getCameraPermissionStatus().then(result => {
-		console.log('get: ' + result);
-		setCameraPermissionStatus(result);
-	});
+	const [qrCode, setQrCode] = useState<Barcode | undefined>(undefined);
 
 	if (cameraPermissionStatus !== 'authorized') {
 		Camera.requestCameraPermission().then(result => {
@@ -43,6 +38,67 @@ const QRScanner = (props: {
 
 	const [frameProcessor, barcodes] = useScanBarcodes([BarcodeFormat.QR_CODE]);
 
+	// check frames for new QR-Codes
+	useEffect(() => {
+		if (Array.isArray(barcodes) && barcodes.length) {
+			const barcode = barcodes[0];
+			if (
+				barcode !== undefined &&
+				barcode.displayValue !== undefined &&
+				barcode.displayValue !== ''
+			) {
+				console.log('qrCodeValue: ' + barcode.displayValue);
+				if (
+					qrCode === undefined ||
+					qrCode.displayValue === '' ||
+					barcode.displayValue !== qrCode.displayValue
+				) {
+					console.log('new QR-Code detected');
+					setQrCode(barcode);
+				}
+			}
+		}
+	}, [barcodes]);
+
+	// process new QR-Code
+	useEffect(() => {
+		if (qrCode !== undefined && qrCode.displayValue !== undefined) {
+			const data = qrCode.displayValue;
+			console.log('qrCode: ' + data);
+			new HandledPromise<[QrCodeData, QrCodeData]>('qr.invalid', resolve => {
+				// validation of parameters
+				if (!props.uid) throw new Error('Torch not yet ready');
+				if (!props.position) throw new Error('Position not accurate enough');
+				if (!data) throw new Error('QR-Code is empty');
+				var self: QrCodeData;
+				var received: QrCodeData;
+				try {
+					self = {
+						ts: Math.floor(Date.now() / 1000),
+						uuid: props.uid,
+						location: props.position,
+					};
+					received = JSON.parse(data);
+				} catch (error) {
+					throw new Error('QR-Code is invalid');
+				}
+				// check timestamp
+				if (self.ts - received.ts > 10) throw new Error('QR-Code is outdated');
+				resolve([self, received]);
+			})
+				.then(([self, received]) =>
+					// POST ApiData to REST API
+					RestClient.postContact(received.uuid, self.uuid, self.location)
+				)
+				.then(() =>
+					// fs -> realm
+					writeUserData({fireStatus: true})
+				);
+			// turn QR-Code Scanner off
+			props.updateQrStatus();
+		}
+	}, [qrCode]);
+
 	if (device == null || cameraPermissionStatus !== 'authorized') return null;
 	return (
 		<>
@@ -51,12 +107,10 @@ const QRScanner = (props: {
 				device={device}
 				isActive={true}
 				frameProcessor={frameProcessor}
-				frameProcessorFps={5}
+				frameProcessorFps={1}
 			/>
-			{barcodes.map((barcode, idx) => (
-				<Text key={idx}>{barcode.displayValue}</Text>
-			))}
-			{/*<View style={styles.headlineBox}>
+			{/*
+			<View style={styles.headlineBox}>
 				<Text style={styles.headlineText}>ILLUMINATE YOUR FIRE!</Text>
 			</View>
 			<View style={styles.window}>
@@ -67,61 +121,24 @@ const QRScanner = (props: {
 				/>
 				
 				<QRCodeScanner
-					onRead={event => {
-						// validation
-						new HandledPromise<[QrCodeData, QrCodeData]>(
-							'qr.invalid',
-							resolve => {
-								if (!props.uid) throw new Error('Torch not yet ready');
-								if (!props.position)
-									throw new Error('Position not accurate enough');
-								if (!event.data) throw new Error('QR-Code is empty');
-								var self: QrCodeData;
-								var received: QrCodeData;
-								try {
-									self = {
-										ts: Math.floor(Date.now() / 1000),
-										uuid: props.uid,
-										location: props.position,
-									};
-									received = JSON.parse(event.data);
-								} catch (error) {
-									throw new Error('QR-Code is invalid');
-								}
-								// check timestamp
-								if (self.ts - received.ts > 10)
-									throw new Error('QR-Code is outdated');
-								resolve([self, received]);
-							}
-						)
-							.then(([self, received]) =>
-								// POST ApiData to REST API
-								RestClient.postContact(
-									received.uuid,
-									self.uuid,
-									self.location.accuracy < received.location.accuracy
-										? self.location
-										: received.location
-								)
-							)
-							.then(() =>
-								// fs -> realm
-								writeUserData({fireStatus: true})
-							);
-						props.updateQrStatus();
-					}}
 					containerStyle={styles.containerView}
 					cameraStyle={styles.cameraView}
 				/>
 				<View style={styles.textBox}>
 					<Text style={styles.text}>Scan a lume QR-Code!</Text>
 				</View>
-				</View>*/}
+				</View>
+				*/}
 		</>
 	);
 };
 
 const styles = StyleSheet.create({
+	barcodeTextURL: {
+		fontSize: 20,
+		color: 'white',
+		fontWeight: 'bold',
+	},
 	headlineBox: {
 		alignItems: 'center',
 		paddingTop: 64,
