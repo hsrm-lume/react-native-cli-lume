b13b38994f598b1df9068ee3775c99f17960b37e
mptr <54647612+mptr@users.noreply.github.com>
2022-01-27 23:51:44 +0100

    try II

--- src/services/InternetCheck.tsx
+++ src/services/InternetCheck.tsx
@@ -1,12 +1,14 @@
 import NetInfo from '@react-native-community/netinfo';
+import {Platform} from 'react-native';
 import {HandledPromise} from '../types';
 
 export const checkConnected = (): HandledPromise<void> => {
 	return new HandledPromise('internet.device', (resolve, reject) => {
 		const unsubscribe = NetInfo.addEventListener(state => {
 			if (unsubscribe) unsubscribe();
-			if (!state.isInternetReachable) reject(state);
-			else resolve();
+			if (Platform.OS == 'ios' ? state.isConnected : state.isInternetReachable)
+				resolve();
+			else reject(state);
 		});
 	});
 };
